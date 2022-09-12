f6da7408892e0604294004a66877b06d21fddda8
Little-Pink-Dragon <111520046+Little-Pink-Dragon@users.noreply.github.com>
2022-09-12 03:08:58 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+{
+  if (a == 6 || a == 7) 
+  {
+  Console.WriteLine("{0} выходной день", a);
+  }
+  else if (a < 1 || a > 7) 
+  {
+    Console.WriteLine("{0} не день недли", a);
+  }
+  else Console.WriteLine("{0} рабочий день", a);
+}
+
